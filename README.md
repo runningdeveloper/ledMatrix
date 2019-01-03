1188a453bbd6fb522c7b76349e536faf389c3e9b
Geoffrey Hunt <geoffrey.hunt.10@gmail.com>
2019-01-04 00:12:07 +0200

    Initial Commit!

---
+++ README.md
@@ -0,0 +1,3 @@
+# Plating with LED Matrix
+
+[100 days of code](https://github.com/runningdeveloper/100-days-of-code)
---
+++ one.js
@@ -0,0 +1,32 @@
+// some testing 
+const five = require("johnny-five"),
+  board = new five.Board();
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+board.on("ready", async () => {
+  console.log('hello board is ready!')
+
+  const matrix = new five.Led.Matrix({
+    pins: {
+      data: 2,
+      clock: 3,
+      cs: 4
+    }
+  });
+
+  matrix.on();
+  matrix.brightness(50);
+
+  // 'hello'.split("").map(async (char)=>{
+  //   await delay(3000)
+  //   matrix.draw(char)
+  // })
+
+  'geoffrey'.split("").forEach((char,i)=>{
+    setTimeout(()=>{
+      matrix.draw(char)
+    }, 1000*i)
+  })
+
+});
